26f43320c382bfd46c10eaf9d830bb6071ef53a0
Jarryd <134438857+jramborger78@users.noreply.github.com>
2023-08-05 08:36:08 -0700

    Update sumay.sh

--- RPi_Codes/sumay.sh
+++ RPi_Codes/sumay.sh
@@ -8,5 +8,6 @@ raspivid -t $ans -br 55 -w 800 -h 600 --framerate $3 -b 10000000 -awb greyworld 
 echo "Converting to mp4"
 MP4Box -add $1_Box02_$now.h264 $1_Box02_$now.mp4 -fps $3
 echo "Transfering..." $(date +%H:%M:%S)
-sshpass -p georgelab1 scp $1_Box02_$now.mp4 georgelab@10.1.1.243:/media/georgelab/Expansion/RPi_Videos_B/Coc_B
+sshpass -p georgelab1 scp $1_Box02_$now.mp4 georgelab@10.1.1.243:/media/georgelab/Expansion/RPi_Videos_B
+sshpass -p georgelab1 scp $1_Box02_$now.h264 georgelab@10.1.1.243:/media/georgelab/Expansion/RPi_Videos_B
 echo "Session complete" $(date +%H:%M:%S)
