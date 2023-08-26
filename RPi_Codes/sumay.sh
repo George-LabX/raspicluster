a685458a3697a5f3d67fa2c043405184abb21e20
Jarryd <134438857+jramborger78@users.noreply.github.com>
2023-08-26 15:00:00 -0700

    Update sumay.sh

--- RPi_Codes/sumay.sh
+++ RPi_Codes/sumay.sh
@@ -5,7 +5,7 @@ ans=$(echo "$2 * 60000" | bc)
 now=$(date "+%F")
 echo "Started recording at" $(date +%H:%M:%S)
 raspivid -t $ans -br 55 -w 800 -h 600 --framerate $3 -b 10000000 -awb greyworld -o $1_Box02_$now.h264
-echo "Converting to mp4" $(date + %H:%M:%S)
+echo "Converting to mp4" $(date +%H:%M:%S)
 MP4Box -add $1_Box02_$now.h264 $1_Box02_$now.mp4 -fps $3
 echo "Transfering..." $(date +%H:%M:%S)
 sshpass -p georgelab1 scp $1_Box02_$now.mp4 georgelab@10.1.1.243:/media/georgelab/Expansion/RPi_Videos_B
