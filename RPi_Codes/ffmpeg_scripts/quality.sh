df704f4523d29e62c9f5b542af8c7be6ea874b55
Jarryd <134438857+jramborger78@users.noreply.github.com>
2023-09-13 08:48:55 -0700

    Update quality.sh

--- RPi_Codes/ffmpeg_scripts/quality.sh
+++ RPi_Codes/ffmpeg_scripts/quality.sh
@@ -4,17 +4,21 @@ ans=$(echo "$2 * 60000" | bc)
 now=$(date "+%F")
 
 echo "Started recording at" $(date +%H:%M:%S)
+raspivid -t $ans -br 55 -w 1280 -h 720 --framerate $3 -b 10000000 -awb greyworld -o $1_Box01_$now.h264
 
-raspivid -t $ans -br 55 -w 1280 -h 720 --framerate $3 -b 10000000 -awb greyworld -o $1_Box02_$now.h264
+echo "Converting to MP4Box" $(date +%H:%M:%S)
+MP4Box -add $1_Box01_$now.h264 MP4Box_$1_Box01_$now.mp4 -fps $3
 
-echo "Converting to mp4" $(date +%H:%M:%S)
+echo "Converting to ffmpeg" $(date +%H:%M:%S)
+ffmpeg -i "$1_Box01_$now.h264" -c:v libx264 -preset slow -tune film -crf 18 "ffmpeg_$1_Box01_$now.mp4"
 
-ffmpeg -i $1_Box02_$now.h264 -c:v h264_nvenc -preset slow -tune hq -profile high -level 4.2 -rc vbr -qp 18 -rc-lookahead 30 $1_Box02_$now.mp4 -r $3
+echo "Transfering h264" $(date +%H:%M:%S)
+sshpass -p georgelab1 scp $1_Box01_$now.h264 georgelab@10.1.1.243:/media/georgelab/Expansion/RPi_Videos_E/Coc_E
 
-echo "Transfering..." $(date +%H:%M:%S)
+echo "Transfering MP4Box" $(date +%H:%M:%S)
+sshpass -p georgelab1 scp MP4Box_$1_Box01_$now.mp4 georgelab@10.1.1.243:/media/georgelab/Expansion/RPi_Videos_E/Coc_E
 
-sshpass -p georgelab1 scp $1_Box02_$now.mp4 georgelab@10.1.1.243:/media/georgelab/Expansion/RPi_Videos_B/Coc_B
-
-rm $1_Box02_$now.h264
+echo "Transfering ffmpeg" $(date +%H:%M:%S)
+sshpass -p georgelab1 scp ffmpeg_$1_Box01_$now.mp4 georgelab@10.1.1.243:/media/georgelab/Expansion/RPi_Videos_E/Coc_E
 
 echo "Session complete" $(date +%H:%M:%S)
