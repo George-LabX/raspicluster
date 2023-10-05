9258bd9e70c3d639a3bf5fbbe215192c9491c9cc
Jarryd <134438857+jramborger78@users.noreply.github.com>
2023-10-05 12:00:17 -0700

    Delete RPi_Codes/remote_codes directory

--- RPi_Codes/remote_codes/fetch.sh
+++
@@ -1,6 +0,0 @@
-# Specific to Row B path
-# Genereic "fetch" executable file that will take one RPi IP address as argument and acquire all mp4 videos from that single RPi
-
-echo "File fetched at" $(date +%H:%M:%S)
-scp pi@$1:/home/pi/*mp4 /media/georgelab/Expansion/RPi_Videos_B
-echo "Fetch complete at" $(date +%H:%M:%S)
