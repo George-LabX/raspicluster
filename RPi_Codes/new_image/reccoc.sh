a1017e8372acfda6e8fa92007f2734ca532b9655
Jarryd <134438857+jramborger78@users.noreply.github.com>
2023-10-13 12:44:08 -0700

    Update reccoc.sh

--- RPi_Codes/new_image/reccoc.sh
+++ RPi_Codes/new_image/reccoc.sh
@@ -6,27 +6,27 @@ now=$(date "+%F")
 
 # Recording Script
 echo "Started recording at:" $(date +%H:%M:%S)
-raspivid -t $ans -br 55 -w 1280 -h 720 --framerate $3 -b 20000000 -awb greyworld -o $1_$Box_$now.h264
+raspivid -t $ans -br 55 -w 1280 -h 720 --framerate $3 -b 20000000 -awb greyworld -o $1_$BOX_$now.h264
 echo -e "\n"
 
 # MP4 Conversion using MP4Box
 echo "Converting to mp4 started at:" $(date +%H:%M:%S)
-MP4Box -add $1_$Box_$now.h264 $1_j$Box_$now.mp4 -fps $3
+MP4Box -add $1_$BOX_$now.h264 $1_j$BOX_$now.mp4 -fps $3
 echo -e "\n"
 
 # FFmpeg "resizing" processing
 echo "FFmpeg processing started at:" $(date +%H:%M:%S)
-ffmpeg -i $1_j$Box_$now.mp4 -vf "scale=1280:720" -loglevel panic $1_$Box_$now.mp4
+ffmpeg -i $1_j$BOX_$now.mp4 -vf "scale=1280:720" -loglevel panic $1_$BOX_$now.mp4
 echo -e "\n"
 
 # Secure copy recording file to remote device
 echo "Transfering started at:" $(date +%H:%M:%S)
-sshpass -p $REMOTEPASS scp $1_$Box_$now.mp4 $REMOTEPATH$MINI/Coc_$MINI
+sshpass -p $REMOTEPASS scp $1_$BOX_$now.mp4 $REMOTE:$REMOTEVIDPATH/Coc_$MINI
 echo -e "\n"
 
 # Clean up of unneeded files
-rm $1_$Box_$now.h264
-rm $1_j$Box_$now.mp4
+rm $1_$BOX_$now.h264
+rm $1_j$BOX_$now.mp4
 
 # Notice of session complete
 echo "Session completed at:" $(date +%H:%M:%S)
