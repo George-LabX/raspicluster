4f2017202c04744c2d9eb85a29e99eb0e8fc65ec
Jarryd <134438857+jramborger78@users.noreply.github.com>
2024-02-13 07:39:04 -0800

    Update mini_osg.sh

--- RPi_Codes/new_image/mini_osg.sh
+++ RPi_Codes/new_image/mini_osg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This code is to transfer video files form a Mini PC to the Super PC.
+# It depends largely on your structure of directories and paths in your user profile.
 # It takes in 3 arguments as follows:
 
 # 1 = Drug, i.e. "Coc"
@@ -8,12 +9,12 @@
 
 if [[ "$3" =~ ^SHOCK[0-9]{2}$ ]]; then
 
-    scp -r /media/george/Expansion/RPi_Videos_B/$1_B/$2_$1_B/$3 jarryd.ramborger@ap40.uw.osg-htc.org:/home/jarryd.ramborger/videos/$1/row_$3/$2/$3
+    scp -r /media/george/Expansion/RPi_Videos_B/$1_B/$2_$1_B/$3 USER.NAME@ap40.uw.osg-htc.org:/home/USER.NAME/videos/$1/row_$3/$2/$3
     
 else
 
 
-    scp -r /media/george/Expansion/RPi_Videos_B/$1_B/$2_$1_B/$3 jarryd.ramborger@ap40.uw.osg-htc.org:/home/jarryd.ramborger/videos/$1/row_B/$2/$3
+    scp -r /media/george/Expansion/RPi_Videos_B/$1_B/$2_$1_B/$3 USER.NAME@ap40.uw.osg-htc.org:/USER.NAME/videos/$1/row_B/$2/$3
 
 fi
 
